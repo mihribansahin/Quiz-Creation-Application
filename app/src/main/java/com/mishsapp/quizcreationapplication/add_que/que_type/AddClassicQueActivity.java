e6ff009277208f0f53b06060a93746cc35551478
mihribansahin <mishsapp@gmail.com>
2021-05-15 00:15:43 +0300

    Classic Que - fileInputOutputStream işlemleri yapıldı

--- app/src/main/java/com/mishsapp/quizcreationapplication/add_que/que_type/AddClassicQueActivity.java
+++ app/src/main/java/com/mishsapp/quizcreationapplication/add_que/que_type/AddClassicQueActivity.java
@@ -1,6 +1,7 @@
 package com.mishsapp.quizcreationapplication.add_que.que_type;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,6 +13,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.mishsapp.quizcreationapplication.R;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 
 public class AddClassicQueActivity extends AppCompatActivity {
     int temp = 0;
@@ -19,17 +33,18 @@ public class AddClassicQueActivity extends AppCompatActivity {
     private EditText editTextClassicQuestion, editTextClassicAnswer, editTextClassicPoint;
     private Button buttonClassicEasy, buttonClassicMiddle, buttonClassicHard, buttonClassicSave;
 
-    String que,answer,grade,point;
+    String que, answer, grade, point;
+    private Context context;
 
-    void init(){
+    void init() {
 
-        editTextClassicQuestion  =findViewById(R.id.editTextClassicQuestion);
-        editTextClassicAnswer  =findViewById(R.id.editTextClassicAnswer);
-        editTextClassicPoint  =findViewById(R.id.editTextClassicPoint);
-        buttonClassicEasy  =findViewById(R.id.buttonClassicEasy);
-        buttonClassicMiddle  =findViewById(R.id.buttonClassictMiddle);
-        buttonClassicHard  =findViewById(R.id.buttonClassicHard);
-        buttonClassicSave  =findViewById(R.id.buttonClassicSave);
+        editTextClassicQuestion = findViewById(R.id.editTextClassicQuestion);
+        editTextClassicAnswer = findViewById(R.id.editTextClassicAnswer);
+        editTextClassicPoint = findViewById(R.id.editTextClassicPoint);
+        buttonClassicEasy = findViewById(R.id.buttonClassicEasy);
+        buttonClassicMiddle = findViewById(R.id.buttonClassictMiddle);
+        buttonClassicHard = findViewById(R.id.buttonClassicHard);
+        buttonClassicSave = findViewById(R.id.buttonClassicSave);
 
     }
 
@@ -73,16 +88,28 @@ public class AddClassicQueActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 myClassicQuestion();
-                Toast.makeText(getApplicationContext(),"SORU KAYDEDİLDİ!", Toast.LENGTH_SHORT).show();
+
+
+                HashMap<String,String> hm;
+                hm=new HashMap<String,String>();
+
+                hm.put("SORU: ",que);
+                hm.put("CEVAP",answer);
+                hm.put("PUAN: ",point);
+                hm.put("ZORLUK DERECESİ: ", grade);
+
+                method2(hm);
+
+                Toast.makeText(getApplicationContext(), "SORU KAYDEDİLDİ!", Toast.LENGTH_SHORT).show();
 
                 editTextClassicQuestion.setText("");
                 editTextClassicAnswer.setText("");
                 editTextClassicPoint.setText("");
 
-                que ="";
-                answer ="";
-                grade ="";
-                point ="";
+                que = "";
+                answer = "";
+                grade = "";
+                point = "";
 
                 buttonClassicEasy.setEnabled(true);
                 buttonClassicMiddle.setEnabled(true);
@@ -92,12 +119,12 @@ public class AddClassicQueActivity extends AppCompatActivity {
     }
 
 
-    private void myClassicQuestion(){
+    private void myClassicQuestion() {
 
-       que = String.valueOf(editTextClassicQuestion.getText());
-       answer = String.valueOf(editTextClassicAnswer.getText());
-       grade = String.valueOf(temp);
-       point = String.valueOf(editTextClassicPoint.getText());
+        que = String.valueOf(editTextClassicQuestion.getText());
+        answer = String.valueOf(editTextClassicAnswer.getText());
+        grade = String.valueOf(temp);
+        point = String.valueOf(editTextClassicPoint.getText());
 
 
         Log.e("KLASİK ", que);
@@ -106,11 +133,61 @@ public class AddClassicQueActivity extends AppCompatActivity {
         Log.e("KLASİK ", point);
 
 
-
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    public void method2(HashMap<String,String> map) {
+
+              /////////////////  WRITE  ///////////////////
+            try {
+                File myFile=new File("questions.txt");
+                FileOutputStream fos= openFileOutput("questions.txt", Context.MODE_PRIVATE);
+                PrintWriter pw=new PrintWriter(fos);
+
+                for(Map.Entry<String,String> m :map.entrySet()){
+                    pw.println(m.getKey()+"="+m.getValue());
+                }
+                pw.flush();
+                pw.close();
+                fos.close();
+                Toast.makeText(getApplicationContext(),"questions.txt" + " saved",
+                        Toast.LENGTH_LONG).show();
+            } catch(Exception e) {
+                Log.e("DOSYA MEVZUSU", e.toString());
+            }
+
+            /////////////////  READ ///////////////////
+
+            try {
+
+                File toRead=new File("questions.txt");
+                FileInputStream fis=new FileInputStream(toRead);
+
+                Scanner sc=new Scanner(fis);
+
+                HashMap<String,String> mapInFile=new HashMap<String,String>();
+
+                //read data from file line by line:
+                String currentLine;
+                while(sc.hasNextLine()) {
+                    currentLine=sc.nextLine();
+                    //now tokenize the currentLine:
+                    StringTokenizer st=new StringTokenizer(currentLine,"=",false);
+                    //put tokens ot currentLine in map
+                    mapInFile.put(st.nextToken(),st.nextToken());
+                }
+                fis.close();
+
+                //print All data in MAP
+                for(Map.Entry<String,String> m :mapInFile.entrySet()) {
+                    System.out.println(m.getKey()+" : "+m.getValue());
+                }
+            }catch(Exception e) {}
+
+
     }
 }
